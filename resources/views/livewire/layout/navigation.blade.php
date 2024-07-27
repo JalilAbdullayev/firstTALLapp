77bf36d1bc24d47067c2d390dc302629e00dbcd1
JalilAbdullayev <abdullacelil00@gmail.com>
2024-07-27 20:14:24 +0400

    Create Landing Page

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -31,6 +31,9 @@ new class extends Component {
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('notes.index')" :active="request()->routeIs('notes.index')" wire:navigate>
+                        Notes
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -90,6 +93,10 @@ new class extends Component {
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('notes.index')" :active="request()->routeIs('notes.index')"
+                                   wire:navigate>
+                Notes
             </x-responsive-nav-link>
         </div>
 
