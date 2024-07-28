12a6736b3723c01299939a5847cf40a354086c05
JalilAbdullayev <abdullacelil00@gmail.com>
2024-07-28 09:27:14 +0400

    Delete a note

--- resources/views/livewire/notes/show-notes.blade.php
+++ resources/views/livewire/notes/show-notes.blade.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Models\Note;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Livewire\Volt\Component;
 
 new class extends Component {
+    public function delete(string $id) {
+        $note = Note::whereId($id)->first();
+        $this->authorize('delete', $note);
+        $note->delete();
+    }
+
     public function with(): array {
         return [
             'notes' => Auth::user()->notes()->orderBy('send_date')->get()
@@ -15,51 +22,52 @@ new class extends Component {
 ?>
 
 <div class="space-y-2">
-    @if($notes->isEmpty())
-        <div class="text-center">
-            <p class="text-xl font-bold">
-                No notes yet
-            </p>
-            <p class="text-sm">
-                Let's create your first note to send.
-            </p>
-            <x-button primary icon-right="plus" class="mt-6" href="{{ route('notes.create') }}" wire:navigate>
-                Create Note
-            </x-button>
-        </div>
-    @else
-        <x-button primary icon-right="plus" class="mb-12" href="{{ route('notes.create') }}" wire:navigate>
-            Create Note
-        </x-button>
-        <div class="grid grid-cols-3 gap-4">
-            @foreach($notes as $note)
-                <x-card wire:key="{{ $note->id }}">
-                    <div class="flex justify-between">
-                        <div>
-                            <a href="" class="text-xl font-bold hover:underline hover:text-blue-500">
-                                {{ $note->title }}
-                            </a>
-                            <p class="text-xs mt-2">
-                                {{ Str::limit($note->body, 50) }}
-                            </p>
-                        </div>
-                        <div class="text-xs text-gray-500">
-                            {{ Carbon::parse($note->send_date)->format('d-M-Y') }}
-                        </div>
-                    </div>
-                    <div class="flex items-center justify-between mt-4 space-x-1">
-                        <p class="text-xs">
-                            Recipient: <span class="font-semibold">
+	@if($notes->isEmpty())
+		<div class="text-center">
+			<p class="text-xl font-bold">
+				No notes yet
+			</p>
+			<p class="text-sm">
+				Let's create your first note to send.
+			</p>
+			<x-button primary icon-right="plus" class="mt-6" href="{{ route('notes.create') }}" wire:navigate>
+				Create Note
+			</x-button>
+		</div>
+	@else
+		<x-button primary icon-right="plus" class="mb-12" href="{{ route('notes.create') }}" wire:navigate>
+			Create Note
+		</x-button>
+		<div class="grid grid-cols-3 gap-4">
+			@foreach($notes as $note)
+				<x-card wire:key="{{ $note->id }}">
+					<div class="flex justify-between">
+						<div>
+							<a href="" class="text-xl font-bold hover:underline hover:text-blue-500">
+								{{ $note->title }}
+							</a>
+							<p class="text-xs mt-2">
+								{{ Str::limit($note->body, 50) }}
+							</p>
+						</div>
+						<div class="text-xs text-gray-500">
+							{{ Carbon::parse($note->send_date)->format('d-M-Y') }}
+						</div>
+					</div>
+					<div class="flex items-center justify-between mt-4 space-x-1">
+						<p class="text-xs">
+							Recipient: <span class="font-semibold">
                             {{ $note->recipient }}
                         </span>
-                        </p>
-                        <div>
-                            <x-mini-button outline secondary rounded icon="eye"></x-mini-button>
-                            <x-mini-button outline secondary rounded icon="trash"></x-mini-button>
-                        </div>
-                    </div>
-                </x-card>
-            @endforeach
-        </div>
-    @endif
+						</p>
+						<div>
+							<x-mini-button outline secondary rounded icon="eye"></x-mini-button>
+							<x-mini-button outline secondary rounded icon="trash"
+										   wire:click="delete('{{ $note->id }}')"></x-mini-button>
+						</div>
+					</div>
+				</x-card>
+			@endforeach
+		</div>
+	@endif
 </div>
